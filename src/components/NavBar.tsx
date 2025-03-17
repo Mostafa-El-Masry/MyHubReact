30f6e69f853ce841ecf6031b9bf1fadd7a38aa1f
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-03-17 21:53:36 +0300

    adding navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,70 @@
 const NavBar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      {/* <!-- NAV BAR --> */}
+
+      <a href="#main-content" className="skip-link">
+        Skip to main content
+      </a>
+
+      <button
+        id="open-sidebar-button"
+        aria-label="open sidebar"
+        aria-expanded="false"
+        aria-controls="navbar"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="40px"
+          viewBox="0 -960 960 960"
+          width="40px"
+          fill="#c9c9c9"
+        >
+          <path d="M165.13-254.62q-10.68 0-17.9-7.26-7.23-7.26-7.23-18t7.23-17.86q7.22-7.13 17.9-7.13h629.74q10.68 0 17.9 7.26 7.23 7.26 7.23 18t-7.23 17.87q-7.22 7.12-17.9 7.12H165.13Zm0-200.25q-10.68 0-17.9-7.27-7.23-7.26-7.23-17.99 0-10.74 7.23-17.87 7.22-7.13 17.9-7.13h629.74q10.68 0 17.9 7.27 7.23 7.26 7.23 17.99 0 10.74-7.23 17.87-7.22 7.13-17.9 7.13H165.13Zm0-200.26q-10.68 0-17.9-7.26-7.23-7.26-7.23-18t7.23-17.87q7.22-7.12 17.9-7.12h629.74q10.68 0 17.9 7.26 7.23 7.26 7.23 18t-7.23 17.86q-7.22 7.13-17.9 7.13H165.13Z" />
+        </svg>
+      </button>
+
+      <nav className="navbar" id="navbar sticky">
+        <ul>
+          <li>
+            <button id="close-sidebar-button" aria-label="close sidebar">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="40px"
+                viewBox="0 -960 960 960"
+                width="40px"
+                fill="#c9c9c9"
+              >
+                <path d="m480-444.62-209.69 209.7q-7.23 7.23-17.5 7.42-10.27.19-17.89-7.42-7.61-7.62-7.61-17.7 0-10.07 7.61-17.69L444.62-480l-209.7-209.69q-7.23-7.23-7.42-17.5-.19-10.27 7.42-17.89 7.62-7.61 17.7-7.61 10.07 0 17.69 7.61L480-515.38l209.69-209.7q7.23-7.23 17.5-7.42 10.27-.19 17.89 7.42 7.61 7.62 7.61 17.7 0 10.07-7.61 17.69L515.38-480l209.7 209.69q7.23 7.23 7.42 17.5.19 10.27-7.42 17.89-7.62 7.61-17.7 7.61-10.07 0-17.69-7.61L480-444.62Z" />
+              </svg>
+            </button>
+          </li>
+          <li className="home-li">
+            <a className="active-link" aria-current="page" href="/">
+              MY HUB
+            </a>
+          </li>
+          <li>
+            <a href="/">Warehouse</a>
+          </li>
+          <li>
+            <a href="/">Library</a>
+          </li>
+          <li>
+            <a href="/">Video Gallery</a>
+          </li>
+          <li>
+            <a href="/">Money</a>
+          </li>
+          <li>
+            <a href="/">Java Script</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div id="overlay" aria-hidden="true"></div>
+    </>
+  );
 };
 
 export default NavBar;
